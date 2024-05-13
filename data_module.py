0601d6f799e97ae9ae9b64ea560f1cf9a1506784
Bilal143260 <smbilal642@gmail.com>
2024-05-13 08:37:46 +0000

    removed indexing and reformatted code

--- data_module.py
+++ data_module.py
@@ -8,8 +8,9 @@ import random
 import numpy as np
 import os
 
-#function copied from:
-#https://github.com/huggingface/diffusers/blob/main/examples/research_projects/dreambooth_inpaint/train_dreambooth_inpaint.py
+
+# function copied from:
+# https://github.com/huggingface/diffusers/blob/main/examples/research_projects/dreambooth_inpaint/train_dreambooth_inpaint.py
 def prepare_mask_and_masked_image(image, mask):
     image = np.array(image.convert("RGB"))
     image = image[None].transpose(0, 3, 1, 2)
@@ -26,6 +27,7 @@ def prepare_mask_and_masked_image(image, mask):
     masked_image = image * (mask < 0.5)
 
     return mask, masked_image
+
 
 class MyDataset(Dataset):
     def __init__(
@@ -50,7 +52,7 @@ class MyDataset(Dataset):
         self.ti_drop_rate = ti_drop_rate
         self.image_root_path = image_root_path
 
-        self.data = json.load(open(json_file))[0:16]
+        self.data = json.load(open(json_file))
 
         self.transforms = transforms.Compose(
             [
@@ -72,7 +74,9 @@ class MyDataset(Dataset):
 
         raw_target_img = Image.open(os.path.join(self.image_root_path, target_img))
         raw_cloth_img = Image.open(os.path.join(self.image_root_path, cloth))
-        raw_mask = Image.open(os.path.join(self.image_root_path, mask)).resize((768,1024))
+        raw_mask = Image.open(os.path.join(self.image_root_path, mask)).resize(
+            (768, 1024)
+        )
         raw_pose = Image.open(os.path.join(self.image_root_path, pose))
 
         cloth_img_tensor = self.transforms(raw_cloth_img.convert("RGB"))
@@ -80,7 +84,9 @@ class MyDataset(Dataset):
         pose_tensor = self.transforms(raw_pose)
 
         target_img_tensor = self.transforms(raw_target_img.convert("RGB"))
-        mask_tensor, masked_img_tensor = prepare_mask_and_masked_image(raw_target_img, raw_mask)
+        mask_tensor, masked_img_tensor = prepare_mask_and_masked_image(
+            raw_target_img, raw_mask
+        )
 
         clip_cloth_img = self.clip_image_processor(
             images=raw_cloth_img, return_tensors="pt"
@@ -137,7 +143,7 @@ class MyDataset(Dataset):
         original_size = torch.tensor([1024, 768])
 
         return {
-            "cloth_image": cloth_img_tensor, #to be used in garment unet
+            "cloth_image": cloth_img_tensor,  # to be used in garment unet
             "target_image": target_img_tensor,  # output
             "pose_image": pose_tensor,
             "text_input_ids_ref": text_input_ids_ref,
@@ -150,7 +156,7 @@ class MyDataset(Dataset):
             "drop_image_embed": drop_image_embed,
             "original_size": original_size,
             "crop_coords_top_left": crop_coords_top_left,
-            "target_size": torch.tensor([1024, 768])
+            "target_size": torch.tensor([1024, 768]),
         }
 
     def __len__(self):
@@ -160,17 +166,27 @@ class MyDataset(Dataset):
 def collate_fn(data):
     cloth_images = torch.stack([example["cloth_image"] for example in data])
     target_images = torch.stack([example["target_image"] for example in data])
-    pose_images =  torch.stack([example["pose_image"] for example in data])
-    text_input_ids_ref = torch.cat([example["text_input_ids_ref"] for example in data],dim=0)
-    text_input_ids_2_ref = torch.cat([example["text_input_ids_2_ref"] for example in data],dim=0)
-    text_input_ids_try = torch.cat([example["text_input_ids_try"] for example in data],dim=0)
-    text_input_ids_2_try = torch.cat([example["text_input_ids_2_try"] for example in data],dim=0)
-    clip_cloth_imgs = torch.cat([example["clip_cloth_img"] for example in data],dim=0)
+    pose_images = torch.stack([example["pose_image"] for example in data])
+    text_input_ids_ref = torch.cat(
+        [example["text_input_ids_ref"] for example in data], dim=0
+    )
+    text_input_ids_2_ref = torch.cat(
+        [example["text_input_ids_2_ref"] for example in data], dim=0
+    )
+    text_input_ids_try = torch.cat(
+        [example["text_input_ids_try"] for example in data], dim=0
+    )
+    text_input_ids_2_try = torch.cat(
+        [example["text_input_ids_2_try"] for example in data], dim=0
+    )
+    clip_cloth_imgs = torch.cat([example["clip_cloth_img"] for example in data], dim=0)
     masked_imgs = torch.cat([example["masked_img"] for example in data])
     masks = torch.cat([example["mask"] for example in data])
     drop_image_embeds = [example["drop_image_embed"] for example in data]
     original_size = torch.stack([example["original_size"] for example in data])
-    crop_coords_top_left = torch.stack([example["crop_coords_top_left"] for example in data])
+    crop_coords_top_left = torch.stack(
+        [example["crop_coords_top_left"] for example in data]
+    )
     target_size = torch.stack([example["target_size"] for example in data])
 
     return {
@@ -187,10 +203,11 @@ def collate_fn(data):
         "drop_image_embeds": drop_image_embeds,
         "original_size": original_size,
         "crop_coords_top_left": crop_coords_top_left,
-        "target_size": target_size
+        "target_size": target_size,
     }
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
 
     from transformers import CLIPTokenizer
 
@@ -198,12 +215,21 @@ if __name__=="__main__":
     json_file = "/home/bilal/datasets/tested_data/data_2.json"
     image_root_path = "/home/bilal/datasets/tested_data"
 
-    tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_name, subfolder="tokenizer")
-    tokenizer_2 = CLIPTokenizer.from_pretrained(pretrained_model_name, subfolder="tokenizer_2")
-    
-    dataset = MyDataset(json_file=json_file, tokenizer=tokenizer, tokenizer_2=tokenizer_2, image_root_path=image_root_path)
+    tokenizer = CLIPTokenizer.from_pretrained(
+        pretrained_model_name, subfolder="tokenizer"
+    )
+    tokenizer_2 = CLIPTokenizer.from_pretrained(
+        pretrained_model_name, subfolder="tokenizer_2"
+    )
 
-    #shapes before dataloader
+    dataset = MyDataset(
+        json_file=json_file,
+        tokenizer=tokenizer,
+        tokenizer_2=tokenizer_2,
+        image_root_path=image_root_path,
+    )
+
+    # shapes before dataloader
     print("------using only dataset--------")
     print(f"Lenght of dataset: {len(dataset)}")
     print(f"Shape of cloth image: {dataset[1]['cloth_image'].shape}")
@@ -218,13 +244,13 @@ if __name__=="__main__":
     print(f"Shape of text_input_ids_2_try: {dataset[1]['text_input_ids_2_try'].shape}")
     print(f"drop_image_embed: {dataset[1]['drop_image_embed']}")
 
-    #shapes after using dataloader without collate function
+    # shapes after using dataloader without collate function
     train_dataloader = torch.utils.data.DataLoader(
         dataset,
         shuffle=True,
         # collate_fn=collate_fn,
         batch_size=4,
-        num_workers=6
+        num_workers=6,
     )
     print("\n------using data loader without collate function--------")
     print(f"Lenght of dataloader with batchsize 4: {len(train_dataloader)}")
@@ -240,15 +266,11 @@ if __name__=="__main__":
         print(f"Shape of text_input_ids_try: {batch['text_input_ids_try'].shape}")
         print(f"Shape of text_input_ids_2_try: {batch['text_input_ids_2_try'].shape}")
         print(f"drop_image_embed: {batch['drop_image_embed']}")
-        break  
-    
-    #shapes after using dataloader with collate function
+        break
+
+    # shapes after using dataloader with collate function
     train_dataloader = torch.utils.data.DataLoader(
-        dataset,
-        shuffle=True,
-        collate_fn=collate_fn,
-        batch_size=4,
-        num_workers=6
+        dataset, shuffle=True, collate_fn=collate_fn, batch_size=4, num_workers=6
     )
     print("\n------using data loader with collate function--------")
     print(f"Lenght of dataloader with batchsize 4: {len(train_dataloader)}")
@@ -264,4 +286,4 @@ if __name__=="__main__":
         print(f"Shape of text_input_ids_try: {batch['text_input_ids_try'].shape}")
         print(f"Shape of text_input_ids_2_try: {batch['text_input_ids_2_try'].shape}")
         print(f"drop_image_embed: {batch['drop_image_embeds']}")
-        break  
+        break
